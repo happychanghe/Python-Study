fbe101769734cd85d94410b48887cdb03accf971
ychanghe <ychanghe@kaist.ac.kr>
2023-05-28 17:11:16 +0900

    nj, matplotlib start

---
+++ testingLearning/matplotlibLearning.py
@@ -0,0 +1,7 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as nppip
+
+fig, ax = plt.subplots()  # Create a figure containing a single axes.
+ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the axes.
+plt.show()
