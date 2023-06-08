f11df6d13a59aacc3686a159c090ee3aa215b626
ychanghe <ychanghe@kaist.ac.kr>
2023-06-08 19:09:33 +0900

    taylor series

---
+++ testingLearning/taylorpolynomial.py
@@ -0,0 +1,50 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import math as m
+
+def nthpolynomial(n: int, k: float, x: np.ndarray, a: float):
+    _yn = np.zeros(len(x))
+    print(type(_yn))
+    for i in range(0, n+1):
+        # print(type(_yn))
+        print(type(k*np.cos(a)*np.power(x-a, i)/m.factorial(i)))
+        _i=i%4
+        if _i==1:
+            _yn = _yn + k*np.cos(a)*np.power(x-a, i)/m.factorial(i)         # why error when using += ??
+        elif _i==2:
+            _yn += -k*np.sin(a)*np.power(x-a, i)/m.factorial(i)
+        elif _i==3:
+            _yn += -k*np.cos(a)*np.power(x-a, i)/m.factorial(i)
+        elif _i==0:
+            _yn += k*np.sin(a)*np.power(x-a, i)/m.factorial(i)
+        else: 
+            print("err!!!!!!!!!!!!!")
+            exit()
+
+    return _yn
+    
+
+_n = 5                 # x domain
+x = np.arange(-_n*np.pi, _n*np.pi, 0.1)
+k = 1                   # y scale
+y1 = k*np.sin(x)        # sin 
+a = 0                   # taylor series from x = a
+# yn = []
+# towhatn = 20
+# for n in range(0, towhatn):
+#     _yn = nthpolynomial(n, k, x)
+#     yn.append(_yn)
+
+fig, axes = plt.subplots(1, 1)
+axes.set_ylim(-2*k, 2*k)
+l1 = axes.plot(x, y1, label='sin')
+# l2; l3; l4; l5
+# for n in range(towhatn):
+#     axes.plot(x, yn[n], label=f'order of {n}')
+axes.plot(x, nthpolynomial(10, k, x, a), label=f'order of {10}')
+axes.plot(x, nthpolynomial(30, k, x, a), label=f'order of {30}')
+axes.legend()
+plt.show()
+
+# TODO: animation 
